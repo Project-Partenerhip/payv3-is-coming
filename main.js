16b4376d33ed911d5d0281b5afeb8652d9e65ee2
plaxswap <plaxswap@gmail.com>
2024-07-27 07:45:35 +0800

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let countDownDate = new Date('Oct 01, 2023 00:00:00').getTime();
+let countDownDate = new Date('Nov 04, 2024 00:00:00').getTime();
 
 let x = setInterval(function () {
   let now = new Date().getTime();
